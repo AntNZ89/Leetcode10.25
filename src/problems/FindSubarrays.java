78782caba0d395b4e5ef9df66752f2d9a9443cbf
AntND <antonlenz508@gmail.com>
2025-10-04 23:45:59 +0200

    Update

---
+++ src/problems/FindSubarrays.java
@@ -0,0 +1,22 @@
+package problems;
+
+import java.util.HashSet;
+
+public class FindSubarrays {
+    public boolean findSubarrays(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0 ; i < nums.length-1 ; i++){
+            if (set.contains(nums[i]+nums[i+1])){
+                return true;
+            }
+            else {
+                set.add(nums[i]+nums[i+1]);
+            }
+        }
+
+        return false;
+
+    }
+}
