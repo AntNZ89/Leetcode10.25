51cb8919a3f2419df56b632e71d794b344bb6e45
AntND <antonlenz508@gmail.com>
2025-10-11 14:06:54 +0200

    Update

---
+++ src/problems/IsPalindrome.java
@@ -0,0 +1,27 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IsPalindrome {
+
+//    public boolean isPalindrome(ListNode head) {
+//
+//        List<Integer> list = new ArrayList<>();
+//
+//        while (head != null){
+//            list.add(head.val);
+//            head = head.next;
+//        }
+//
+//        for (int i = 0 ; i < list.size() ; i++){
+//            if (list.get(i) != list.get(list.size()-i-1)){
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//
+//    }
+
+}
