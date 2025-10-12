72db94d34f64fb3b7794d1a3481c31ba4d930a5a
AntND <antonlenz508@gmail.com>
2025-10-12 22:18:49 +0200

    Update

---
+++ src/problems/TransformArray.java
@@ -0,0 +1,22 @@
+package problems;
+
+import java.util.Arrays;
+
+public class TransformArray {
+    public int[] transformArray(int[] nums) {
+
+        for (int i = 0 ; i < nums.length ; i++){
+            if (nums[i] % 2 == 0){
+                nums[i] = 0;
+            }
+            else {
+                nums[i] = 1;
+            }
+        }
+
+        Arrays.sort(nums);
+
+        return nums;
+
+    }
+}
