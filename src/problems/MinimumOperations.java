5877bfcf4f2ced243aa6e602702c64e2c50cbb72
AntND <antonlenz508@gmail.com>
2025-10-13 13:38:33 +0200

    Update

---
+++ src/problems/MinimumOperations.java
@@ -0,0 +1,17 @@
+package problems;
+
+public class MinimumOperations {
+    public int minimumOperations(int[] nums) {
+
+        int ans = 0;
+
+        for (int i = 0 ; i < nums.length ; i++){
+            if (nums[i] % 3 != 0){
+                ans++;
+            }
+        }
+
+        return ans;
+
+    }
+}
